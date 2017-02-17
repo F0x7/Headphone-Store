fa6d8ddd887039636604f75fa658dc3ca5ff8ba7
F0x7 <Fox@macbook-pro-2.home>
2017-02-17 22:36:49 +0100

    Initial Commit

---
+++ Headphone Store/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Headphone Store
+//
+//  Created by George Bakaykin on 17/02/2017.
+//  Copyright © 2017 Egor Bakaykin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Headphone Store/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Headphone Store
+//
+//  Created by George Bakaykin on 17/02/2017.
+//  Copyright © 2017 Egor Bakaykin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
